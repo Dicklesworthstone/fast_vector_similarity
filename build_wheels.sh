b4debf9b91229f1bbc49c3ccfc0512149dd34aca
JE <jeff141421@gmail.com>
2023-08-21 23:45:00 -0500

    updated build_wheels.sh

--- build_wheels.sh
+++ build_wheels.sh
@@ -5,11 +5,15 @@ set -e -x
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source $HOME/.cargo/env
 
+# Python versions to build for
+PYTHON_VERSIONS=("cp36-cp36m" "cp37-cp37m" "cp38-cp38" "cp39-cp39" "cp310-cp310" "cp311-cp311")
+
 # Compile wheels
-for PYBIN in /opt/python/cp3[6-9]*; do
-    "${PYBIN}/bin/pip" install --upgrade pip
-    "${PYBIN}/bin/pip" install maturin
-    "${PYBIN}/bin/maturin" build --release
+for PYVER in "${PYTHON_VERSIONS[@]}"; do
+    PYBIN="/opt/python/${PYVER}/bin"
+    "${PYBIN}/pip" install --upgrade pip
+    "${PYBIN}/pip" install maturin
+    "${PYBIN}/maturin" build --release
 done
 
 # Copy the wheels to the shared volume
